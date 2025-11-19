721de359aea855574b683da7cfffbee52efb66fe
Elangkavi <elangakavik3@gmail.com>
2025-11-19 09:54:04 +0530

    Updated package.json and vite.config.ts for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/EmpFrontend/", // 👈 IMPORTANT
+  base: "/EmpFrontend/",
+  server :{
+    port : 3001,
+    open : true
+  }
 })
